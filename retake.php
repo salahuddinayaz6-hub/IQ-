401d3c577d0aeca67de9a7ae11d74d3b2fc99f73
salahuddinayaz6-hub <salahuddinayaz6@gmail.com>
2026-03-06 07:24:55 +0500

    Create retake.php

---
+++ retake.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Resetting...</title>
+</head>
+<body>
+    <script>
+        // Using JS for redirection as requested
+        window.location.href = 'index.php';
+    </script>
+</body>
+</html>
+ 
